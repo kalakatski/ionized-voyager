08e3b760836106821b37c0ffd654c82c81fdcb9a
kalakatski <joshuacherian@gmail.com>
2026-01-30 20:01:47 +0530

    Fix West region cities: Add Navi Mumbai, remove incorrect cities

--- src/utils/cityValidation.js
+++ src/utils/cityValidation.js
@@ -40,13 +40,8 @@ const REGION_CITY_MAP = {
         'Mumbai',
         'Pune',
         'Ahmedabad',
-        'Surat',
-        'Nagpur',
-        'Indore',
-        'Bhopal',
         'Goa',
-        'Nashik',
-        'Aurangabad'
+        'Navi Mumbai'
     ]
 };
 
